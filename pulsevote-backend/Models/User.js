e87d32485388f4907bc61ba79f5fb869e178c095
Zario Di Paolo <st10441349@myemeris.edu.za>
2026-07-29 16:14:07 +0200

    Updated
    Updated code including the next parts of the document number 6 and 7

--- pulsevote-backend/Models/User.js
+++ pulsevote-backend/Models/User.js
@@ -1,19 +1,25 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
- 
+
+const roleSchema = new mongoose.Schema({
+  organisationId: { type: mongoose.Schema.Types.ObjectId, ref: "Organisation" },
+  role: { type: String, enum: ["admin", "manager", "user"], required: true }
+}, { _id: false });
+
 const userSchema = new mongoose.Schema({
   email: { type: String, unique: true, required: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  roles: [roleSchema]
 });
- 
+
 userSchema.pre("save", async function () {
-  if (!this.isModified("password")) return ;
+  if (!this.isModified("password")) return;
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
- 
+
 userSchema.methods.comparePassword = function (candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
 };
- 
+
 module.exports = mongoose.model("User", userSchema);
